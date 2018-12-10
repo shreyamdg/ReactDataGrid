2d364dda8eccd651c38943edd8f76c687122c5d5
shreyam15 <shreyam.duttagupta@gmail.com>
2018-12-10 14:25:18 +0530

    adding code to try error boundaries

---
+++ scripts/editing.js
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import ReactDataGrid from "react-data-grid";
+import { Toolbar, Data } from "react-data-grid-addons";
+import createRowData from "./createRowData";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, info) {
+    // You can also log the error to an error reporting service
+    logErrorToMyService(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+const defaultColumnProperties = {
+  filterable: true,
+  width: 120
+};
+
+const selectors = Data.Selectors;
+
+const columns = [
+  {
+    key: "id",
+    name: "ID"
+  },
+  {
+    key: "title",
+    name: "Title"
+  },
+  {
+    key: "firstName",
+    name: "First Name"
+  },
+  {
+    key: "lastName",
+    name: "Last Name"
+  },
+  {
+    key: "email",
+    name: "Email"
+  },
+  {
+    key: "street",
+    name: "Street"
+  },
+  {
+    key: "zipCode",
+    name: "ZipCode"
+  },
+  {
+    key: "date",
+    name: "Date"
+  },
+  {
+    key: "jobTitle",
+    name: "Job Title"
+  },
+  {
+    key: "catchPhrase",
+    name: "Catch Phrase"
+  },
+  {
+    key: "jobArea",
+    name: "Job Area"
+  },
+  {
+    key: "jobType",
+    name: "Job Type"
+  }
+].map(c => ({ ...c, ...defaultColumnProperties }));
+
+const ROW_COUNT = 50;
+
+const handleFilterChange = filter => filters => {
+  const newFilters = { ...filters };
+  if (filter.filterTerm) {
+    newFilters[filter.column.key] = filter;
+  } else {
+    delete newFilters[filter.column.key];
+  }
+  return newFilters;
+};
+
+function getRows(rows, filters) {
+  return selectors.getRows({ rows, filters });
+}
+
+function Example({ rows }) {
+  const [filters, setFilters] = useState({});
+  const filteredRows = getRows(rows, filters);
+  return (
+
+    <ReactDataGrid
+      columns={columns}
+      rowGetter={i => filteredRows[i]}
+      rowsCount={filteredRows.length}
+      minHeight={500}
+      toolbar={<Toolbar enableFilter={true} />}
+      onAddFilter={filter => setFilters(handleFilterChange(filter))}
+      onClearFilters={() => setFilters({})}
+    />
+
+  );
+}
+
+
+
+window.onload = function(){
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(<ErrorBoundary>
+    <Example rows={createRowData(50)} />
+    </ErrorBoundary>
+    , rootElement);
+}
